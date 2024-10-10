123cc8701e72e3cdeccde3bda9df6b63423cdd0d
AmauriBlanco <blancoamauri@gmail.com>
2024-10-10 16:40:07 -0300

    1-5 Trabalhando com multiplas props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script setup>
-import Titulo from './components/Titulo.vue';
+import Titulo from "./components/Titulo.vue";
+import CardProfessores from "./components/CardProfessores.vue";
 </script>
 
 <template>
-    <Titulo texto="Treinaweb"/>
+    <Titulo texto="Professores" />
 
-    <Titulo texto="Teste 2"/>
-
-    <Titulo texto="teste de título 3"/>
-
+    <div class="card-container">
+        <CardProfessores
+            nome="Elton"
+            avatar="https://github.com/elton-fonseca.png"
+            linguagem="PHP"
+        />
+        <CardProfessores
+            nome="Amauri"
+            avatar="https://www.github.com/AmauriBlanco.png"
+            linguagem="JavaScript"
+        />
+        <CardProfessores
+            nome="Cleyson"
+            avatar="https://github.com/CleysonPH.png"
+            linguagem="Python"
+        />
+    </div>
 </template>
+
+<style scoped>
+.card-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+</style>
---
+++ src/components/CardProfessores.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+    nome: String,
+    avatar: String,
+    linguagem: String,
+});
+</script>
+
+<template>
+    <div class="card">
+        <img :src="avatar" :alt="none" width="150px" />
+        <h3>{{ nome }}</h3>
+        <p>Linguagem: {{ linguagem }}</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: #2b2b2b;
+    padding: 1.5rem;
+    border-radius: 10px;
+    max-width: 200px;
+    text-align: center;
+}
+
+img {
+    border-radius: 50%;
+    border: solid 3px green;
+}
+
+h3 {
+    color: #fff;
+    font-size: 1.5rem;
+}
+</style>
